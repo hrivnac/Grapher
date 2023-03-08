306924063c58b897def8f8ae2ddea235e4dd61a5
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-03-08 16:49:54 +0100

    simple convertor started

--- src/java/com/Grapher/Apps/CLI.java
+++ src/java/com/Grapher/Apps/CLI.java
@@ -1,5 +1,8 @@
 package com.Grapher.Apps;
 
+import com.Grapher.Convertors.Convertor;
+import com.Grapher.Utils.Init;
+
 // CLI
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.BasicParser;
@@ -22,76 +25,96 @@ import org.apache.log4j.Logger;
   * @opt types
   * @opt visibility
   * @author <a href="mailto:Julius.Hrivnac@cern.ch">J.Hrivnac</a> */
-public abstract class CLI {
+public class CLI {
 
+  public static void main(String[] args) {
+    CLI cli = new CLI();
+    cli.parseArgs(args);
+    Init.init(cli.quiet());
+    cli.execute();
+    }
+  
   /** Create. */
   public CLI() {}
 
-  /** Execute command line in required way.
-    * @return The execution reesult. */
-  public abstract String execute();
-  
-  /** Close. */
-  public abstract void close();
-    
-  /** Parse the cli arguments.
-    * @param args    The cli arguments.
-    * @param helpMsg The general help message.
-    * @return        The parsed {@link CommandLine}. */
-  public static CommandLine parseArgs(String[] args,
-                                      String   helpMsg) {
-    return parseArgs(args, helpMsg, null);
+  /** Execute command line in required way. */
+  public void execute() {
+    Convertor convertor = new Convertor(_infile, _outfile);
+    convertor.convert();
     }
     
   /** Parse the cli arguments.
-    * @param args    The cli arguments.
-    * @param helpMsg The general help message.
-    * @param options The already initialised {@link Options}. May be <tt>null</tt>.
-    * @return        The parsed {@link CommandLine}. */
-  public static CommandLine parseArgs(String[] args,
-                                      String   helpMsg,
-                                      Options  options) {
+    * @param args    The cli arguments. */
+  public void parseArgs(String[] args) {
     CommandLineParser parser = new BasicParser();
-    if (options == null) {
-      options = new Options();
-      }
+    Options options = new Options();
     options.addOption("h", "help",     false, "show help");
     options.addOption("q", "quiet",    false, "minimal direct feedback");
+    options.addOption(OptionBuilder.withLongOpt("in")
+                                   .withDescription("input file name")
+                                   .hasArg()
+                                   .withArgName("infile")
+                                   .create("i"));
+    options.addOption(OptionBuilder.withLongOpt("out")
+                                   .withDescription("output file name")
+                                   .hasArg()
+                                   .withArgName("outfile")
+                                   .create("o"));
     try {
       CommandLine cline = parser.parse(options, args );
       if (cline.hasOption("quiet")) {
         _quiet = true;
         }
       if (cline.hasOption("help")) {
-        StringWriter out    = new StringWriter();
-        PrintWriter  writer = new PrintWriter(out);
-        new HelpFormatter().printHelp(writer, 80, helpMsg, "", options, 0, 0, "", true);
-        writer.flush();
-        _help = out.toString();
+        new HelpFormatter().printHelp("java -jar Grapher.exe.jar", options);
+        System.exit(0);
         }
-      return cline;
+      if (cline.hasOption("in")) {
+        _infile = cline.getOptionValue("in");
+        }
+      else {
+        log.error("No input file specified"); 
+        new HelpFormatter().printHelp("java -jar Grapher.exe.jar", options);
+        System.exit(0);
+        }
+      if (cline.hasOption("out")) {
+        _outfile = cline.getOptionValue("out");
+        }
       }
     catch (ParseException e) { 
-      new HelpFormatter().printHelp(helpMsg, options);
-      return null;
+      new HelpFormatter().printHelp("java -jar Grapher.exe.jar", options);
+      System.exit(0);
       }
     }
-   
-  /** Tell whether running in a quiet mode.
-    * @return Whether running in a quiet mode. */
-  public static boolean quiet() {
-    return _quiet;
-   }
    
    /** Give the help.
     * @return The help. */
   public static String help() {
     return _help;
    }
-                                     
-  private static String  _help       = "";
+   
+  /** Tell whether running in a quiet mode.
+    * @return Whether running in a quiet mode. */
+  public boolean quiet() {
+    return _quiet;
+   }
+    
+  /** Give the input file name.
+    * @return The input file name. */
+  public String infile() {
+    return _infile;
+   }
+    
+  /** Give the output file name.
+    * @return The output file name. */
+  public String outfile() {
+    return _outfile;
+   }
                                     
-  private static boolean _quiet      = false;
+  private static String  _help       = "";                                    
+  private        boolean _quiet      = false;
+  private        String  _infile;
+  private        String  _outfile;
 
   /** Logging . */
   private static Logger log = Logger.getLogger(CLI.class);
